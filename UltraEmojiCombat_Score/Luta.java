94509cdb9eea7dc98630d1a5447648019be42a9c
antsu comuna <acampospsantos@hotmail.com>
2022-08-22 18:51:21 -0300

    UEC_Score

--- UltraEmojiCombat_Score/Luta.java
+++ UltraEmojiCombat_Score/Luta.java
@@ -1,5 +1,143 @@
 package UltraEmojiCombat_Score;
 
-public class Luta { //Classe da Luta - envolvendo os métodos essenciais para que a Luta ocorra
+public class Luta { // Classe da Luta - envolvendo os métodos essenciais para que a Luta ocorra
+
+    // Atributos da Classe Luta(Encapsulados)
+    private Lutador desafiador;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
+
+    // MÉTODO CONSTRUTOR
+    public Luta() {
+        this.setAprovada(false);
+    }
+
+
+    // Métodos Acessores e Modificadores(get's e set's)
+    public int getRounds() {
+        return this.rounds;
+    }
+
+    private void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+    public boolean getAprovada() {
+        return this.aprovada;
+    }
+
+
+    // Métodos da Classe Luta
     
+    public void criterios(Lutador desafiador, Lutador desafiante) {
+        desafiador.setPontos(0);
+        desafiante.setPontos(0);
+
+        //****** CRITÉRIO: NÚMERO DE PARTIDAS (EXPERIÊNCIA) ******
+        if (desafiador.getPartidas() > desafiante.getPartidas()) {
+            desafiador.setPontos(desafiador.getPontos() + 1);
+        } else if (desafiante.getPartidas() > desafiador.getPartidas()) {
+            desafiante.setPontos(desafiante.getPontos() + 1);
+        } else { //Caso lutadores possuam mesmo número de Partidas...
+            //CRITÉRIO DESEMPATE: NÚMERO DE VITÓRIAS
+            if (desafiador.getVitorias() > desafiante.getVitorias()) {
+                desafiador.setPontos(desafiador.getPontos() + 1);
+            } else if (desafiante.getVitorias() > desafiador.getVitorias()) {
+                desafiante.setPontos(desafiante.getPontos() + 1);
+            } else { // Caso lutadores possuam mesmo número de Vitórias...
+                //CRITÉRIO DESEMPATE: NÚMERO DE DERROTAS
+                if (desafiador.getDerrotas() > desafiante.getDerrotas()) {
+                    desafiador.setPontos(desafiador.getPontos() - 1);
+                } else if (desafiante.getDerrotas() > desafiador.getDerrotas()) {
+                    desafiante.setPontos(desafiante.getPontos() - 1);
+                } else {
+                    //Ambos NÃO PONTUAM
+                }
+            }
+        }
+        //****** CRITÉRIO: ALTURA ******
+        if (desafiador.getAltura() > desafiante.getAltura()) {
+            desafiador.setPontos(desafiador.getPontos() + 1);
+        } else if (desafiante.getAltura() > desafiador.getAltura()) {
+            desafiante.setPontos(desafiante.getPontos() + 1);
+        } else { //Caso lutadores possuam mesma altura...
+            //CRITÉRIO DESEMPATE: PESAGEM
+            if (desafiador.getPeso() > desafiante.getPeso()) {
+                desafiador.setPontos(desafiador.getPontos() + 1);
+            } else if (desafiante.getPeso() > desafiador.getPeso()) {
+                desafiante.setPontos(desafiante.getPontos() + 1);
+            } else {
+                //Ambos NÃO PONTUAM
+            }
+        }
+        //****** CRITÉRIO: IDADE ******
+        if (desafiador.getIdade() > 30 && desafiador.getIdade() < 40) {
+            desafiador.setPontos(desafiador.getPontos() + 1);
+        }
+        if (desafiante.getIdade() > 30 && desafiante.getIdade() < 40) {
+            desafiante.setPontos(desafiante.getPontos() + 1);
+        }
+    }
+    
+    public void marcarLuta(Lutador desafiador, Lutador desafiante) {
+        if (desafiador.getApto() == true && desafiante.getApto() == true) {
+            if (desafiador.getCategoria().equals(desafiante.getCategoria())) {
+                if (desafiador != desafiante) {
+                    this.setAprovada(true);
+                    System.out.println("----------");
+                    System.out.println("## LUTA APROVADA ##");
+                    System.out.println("----------");
+                } else {
+                    this.setAprovada(false);
+                    System.out.println("----------");
+                    System.out.println("## LUTA NÃO APROVADA ##");
+                    System.out.println("## Desafiante e Desafiador são a mesma pessoa ##");
+                    System.out.println("----------");
+                }
+            } else {
+                this.setAprovada(false);
+                System.out.println("----------");
+                System.out.println("## LUTA NÃO APROVADA ##");
+                System.out.println("## LUTADORES DE DIFERENTES CATEGORIAS ##");
+                System.out.println("----------");
+            }
+        } else {
+            this.setAprovada(false);
+            System.out.println("----------");
+            System.out.println(" ## LUTA NAO AUTORIZADA ##");
+            System.out.println("----------");
+        }
+    }
+
+    public void lutar(Lutador desafiador, Lutador desafiante) {
+        if (this.getAprovada() == true) {
+            System.out.println("");
+            System.out.println("----- LUTA -----");
+            System.out.println(desafiador.getNome() + " x " + desafiante.getNome());
+            desafiador.apresentar();
+            desafiante.apresentar();
+
+            // CASO LÓGICO (PONTOS)
+            System.out.println("CASO LÓGICO (PONTOS)");
+            criterios(desafiador, desafiante);
+            if (desafiador.getPontos() > desafiante.getPontos()) {
+                System.out.println("## VITÓRIA DO DESAFIADOR (" + desafiador.getNome() + ") ##");                
+                desafiador.ganharLuta();
+                desafiante.perderLuta();
+            } else if (desafiante.getPontos() > desafiador.getPontos()) {
+                System.out.println("## VITÓRIA DO DESAFIANTE (" + desafiante.getNome() + ") ##");                
+                desafiador.perderLuta();
+                desafiante.ganharLuta();
+            } else {
+                System.out.println("## EMPATE ##");
+                desafiador.empatarLuta();
+                desafiante.empatarLuta();
+            }
+            System.out.println("PONTUAÇÃO: " + desafiador.getNome() + " " + desafiador.getPontos() + "  X  " + desafiante.getPontos() + " " + desafiante.getNome());
+
+        } else { // this.getAprovada() == false;
+            System.out.println("## LUTA NÃO MARCADA ##");
+        }
+
+    }
 }
--- UltraEmojiCombat_Score/Lutador.java
+++ UltraEmojiCombat_Score/Lutador.java
@@ -2,4 +2,206 @@ package UltraEmojiCombat_Score;
 
 public class Lutador { //Classe do Lutador - Atributos e Métodos próprios de cada lutador
     
+    //Atributos (Encapsulados)
+    private String nome; 
+    private String nacionalidade; 
+    private int idade;
+    private double altura; 
+    private double peso; 
+    private String categoria; 
+    private int vitorias; 
+    private int derrotas; 
+    private int empates; 
+    private int partidas; 
+    private boolean apto; 
+    private int pontos;    
+    
+    
+    //Método Construtor
+    public Lutador(String nome, String nacionalidade, int idade, double altura, double peso, int vitorias, int derrotas, int empates) {
+        this.setNome(nome);
+        this.setNacionalidade(nacionalidade);
+        this.setIdade(idade);
+        this.setAltura(altura);
+        this.setPeso(peso);
+        this.setCategoria(this.getPeso());
+        this.setVitorias(vitorias);
+        this.setDerrotas(derrotas);
+        this.setEmpates(empates);
+        //
+        this.setPartidas(vitorias, derrotas, empates);        
+        this.setApto(true);
+        this.setPontos(0);  
+    }
+
+    //Métodos Acessores e Modificadores
+    private void setNome(String nome) {
+        if (nome != null && nome != "") { //(!nome.equals(null) && !nome.equals(""));
+            this.nome = nome;
+        } else {
+            this.setApto(false);
+        }
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    
+    private void setNacionalidade(String nacionalidade) {
+        if (nome != null && nome != "") {
+            this.nacionalidade = nacionalidade;
+        } else {
+            this.setApto(false);
+        }
+    }
+    public String getNacionalidade() {
+        return this.nacionalidade;
+    }
+    
+    private void setIdade(int idade) {
+        if (idade > 20 && idade < 60) {
+            this.idade = idade;
+        } else {
+            this.setApto(false);
+        }
+    }
+    public int getIdade() {
+        return this.idade;
+    }
+
+    //Unidade: metros(m)
+    private void setAltura(double altura) {
+        if (altura > 1 && altura < 3) {
+            this.altura = altura;
+        } else {
+            this.setApto(false);
+        }           
+    }
+    public double getAltura() {
+        return this.altura;
+    }
+    
+    private void setPeso(double peso) {
+        this.peso = peso;
+        this.setCategoria(peso);
+    }
+    public double getPeso() {
+        return this.peso;
+    }
+    
+    private void setCategoria(double peso) {
+        if (peso < 52.2) {
+            this.categoria = "Inválido";
+            this.setApto(false);
+        } else if (peso <= 70.3) {
+            this.categoria = "Leve";
+        } else if (peso <= 83.9) {
+            this.categoria = "Médio";
+        } else if (peso <= 120.2) {
+            this.categoria = "Pesado";
+        } else {
+            this.categoria = "Inválido";
+            this.setApto(false);
+        }
+    }
+    public String getCategoria() {
+        return this.categoria;
+    }
+    
+    private void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+    public int getVitorias() {
+        return this.vitorias;
+    }
+    
+    private void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+    public int getDerrotas() {
+        return this.derrotas;
+    }
+    
+    private void setEmpates(int empates) {
+        this.empates = empates;
+    }
+    public int getEmpates() {
+        return this.empates;
+    }
+    
+    private void setPartidas(int vitorias, int derrotas, int empates) {
+        this.partidas = vitorias + derrotas + empates;
+    }
+    public int getPartidas() {
+        return this.partidas;
+    }
+        
+    private void setApto(boolean apto) {
+        this.apto = apto;
+    }
+    public boolean getApto() {
+        return this.apto;
+    }    
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }    
+    public int getPontos(){
+        return this.pontos;
+    }
+    
+
+    
+    
+    //Métodos da Classe
+    public void apresentar() {
+        if (this.getApto() == true) {
+            System.out.println("");
+            System.out.println("----- APRESENTAÇÃO -----");
+            System.out.println("Nome do Lutador: " + this.getNome());
+            System.out.println("Nacionalidade: " + this.getNacionalidade());
+            System.out.println("Idade: " + this.getIdade());
+            System.out.println("Altura: " + this.getAltura() + "m");
+            System.out.println("Categoria: " + this.getCategoria());
+            System.out.println("Ganhou: " + this.getVitorias() + " partidas");
+            System.out.println("Perdeu: " + this.getDerrotas() + " partidas");
+            System.out.println("Empatou: " + this.getEmpates() + " partidas");
+            System.out.println("TOTAL: " + this.getPartidas() + " partidas");
+            System.out.println("----------------------");
+            System.out.println("");
+        } else {
+            System.out.println("null");
+        }
+    }
+
+    public void status() {
+        if (this.getApto() == true) {
+            System.out.println("");
+            System.out.println("----- STATUS -----");
+            System.out.println("Nome do Lutador: " + this.getNome());
+            System.out.println("Categoria: " + this.getCategoria());
+            System.out.println("Ganhou: " + this.getVitorias() + " partidas");
+            System.out.println("Perdeu: " + this.getDerrotas() + " partidas");
+            System.out.println("Empatou: " + this.getEmpates() + " partidas");
+            System.out.println("TOTAL: " + this.getPartidas() + " partidas");
+            System.out.println("---------------");
+            System.out.println("");
+        } else {
+            System.out.println("null");
+        }
+    }
+
+    public void ganharLuta() {
+        this.setVitorias(this.getVitorias() + 1);
+        this.partidas = this.partidas + 1;
+    }
+
+    public void perderLuta() {
+        this.setDerrotas(this.getDerrotas() + 1);
+        this.partidas = this.partidas + 1;
+    }
+
+    public void empatarLuta() {
+        this.setEmpates(this.getEmpates() + 1);
+        this.partidas = this.partidas + 1;
+    }
 }
