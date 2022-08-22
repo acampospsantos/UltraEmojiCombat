ed18ad79ad4411834fdce8eeb00dc7a57358a6ee
antsu comuna <acampospsantos@hotmail.com>
2022-08-22 17:03:45 -0300

    UEC_Random

--- UltraEmojiCombat_Random/Luta.java
+++ UltraEmojiCombat_Random/Luta.java
@@ -1,5 +1,99 @@
 package UltraEmojiCombat_Random;
 
-public class Luta{ //Classe da Luta - envolvendo os métodos essenciais para que a Luta ocorra
+import java.util.Random;
+
+public class Luta { // Classe da Luta - envolvendo os métodos essenciais para que a Luta ocorra
+
+    // Atributos da Classe Luta(Encapsulados)
+    private Lutador desafiador;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
+
+    // MÉTODO CONSTRUTOR
+    public Luta() {
+        this.setAprovada(false);
+    }
+
     
+    // Métodos Acessores e Modificadores(get's e set's)
+    public int getRounds() {
+        return this.rounds;
+    }
+
+    private void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+    public boolean getAprovada() {
+        return this.aprovada;
+    }
+
+
+    // Métodos da Classe Luta
+    public void marcarLuta(Lutador desafiador, Lutador desafiante) {
+        if (desafiador.getApto() == true && desafiante.getApto() == true) {
+            if (desafiador.getCategoria().equals(desafiante.getCategoria())) {
+                if (desafiador != desafiante) {
+                    this.setAprovada(true);
+                    System.out.println("----------");
+                    System.out.println("## LUTA APROVADA ##");
+                    System.out.println("----------");
+                } else {
+                    this.setAprovada(false);
+                    System.out.println("----------");
+                    System.out.println("## LUTA NÃO APROVADA ##");
+                    System.out.println("## Desafiante e Desafiador são a mesma pessoa ##");
+                    System.out.println("----------");
+                }
+            } else {
+                this.setAprovada(false);
+                System.out.println("----------");
+                System.out.println("## LUTA NÃO APROVADA ##");
+                System.out.println("## LUTADORES DE DIFERENTES CATEGORIAS ##");
+                System.out.println("----------");
+            }
+        } else {
+            this.setAprovada(false);
+            System.out.println("----------");
+            System.out.println(" ## LUTA NAO AUTORIZADA ##");
+            System.out.println("----------");
+        }
+    }
+
+    public void lutar(Lutador desafiador, Lutador desafiante) {
+        if (this.getAprovada() == true) {
+            System.out.println("");
+            System.out.println("----- LUTA -----");
+            System.out.println(desafiador.getNome() + " x " + desafiante.getNome());
+            desafiador.apresentar();
+            desafiante.apresentar();
+
+            // CASO ALEATÓRIO (SIMULAÇÃO)
+            System.out.println("CASO ALEATÓRIO (SIMULAÇÃO)");
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3); // 0 1 2
+            switch (vencedor) {
+                case 0:
+                    System.out.println("## EMPATE ##");
+                    desafiador.empatarLuta();
+                    desafiante.empatarLuta();
+                    break;
+                case 1:
+                    System.out.println("## VITÓRIA DO DESAFIADOR (" + desafiador.getNome() + ") ##");
+                    desafiador.ganharLuta();
+                    desafiante.perderLuta();
+                    break;
+                case 2:
+                    System.out.println("## VITÓRIA DO DESAFIANTE (" + desafiante.getNome() + ") ##");
+                    desafiador.perderLuta();
+                    desafiante.ganharLuta();
+                    break;
+            }
+
+        } else { // this.getAprovada() == false;
+            System.out.println("## LUTA NÃO MARCADA ##");
+        }
+
+    }
+
 }
--- UltraEmojiCombat_Random/UltraEmojiCombat.java
+++ UltraEmojiCombat_Random/UltraEmojiCombat.java
@@ -2,8 +2,32 @@ package UltraEmojiCombat_Random;
 
 public class UltraEmojiCombat { //Classe Principal
 
-    public static void main(String[] args) {       
+    public static void main(String[] args) {      
+        
+        //Instância da Classe Luta
+        Luta fight = new Luta();        
+        
+        // Instância da Classe Lutador - Array de 6 Objetos (6 Lutadores)
+        //Lutador(nome, Nacionalidade, Idade, Altura, Peso, Vitorias, Derrotas, Empates)
+        Lutador []l = new Lutador[6];
+              
+        l[0] = new Lutador("Chief Altair", "França", 31, 1.75, 68.9, 11, 2, 1);
        
+        l[1] = new Lutador("Jack", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+       
+        l[2] = new Lutador("Logan", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+       
+        l[3] = new Lutador("Michael", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+       
+        l[4] = new Lutador("Vision", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+       
+        l[5] = new Lutador("Wando", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+        
+    
+        fight.marcarLuta(l[0], l[1]);
+        fight.lutar(l[0], l[1]);
+        //l[0].status();
+        //l[0].status();
     }
     
 }
